6684c64c6882b65844f3f11d8aabf668c8785f43
Ayush <68819610+akothule@users.noreply.github.com>
2025-03-16 12:56:01 -0700

    Finished nearest neighbor algorithm

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import pandas as pd
 
@@ -7,33 +8,190 @@ def load_data(file_name):
     data = np.loadtxt(file_name)
 
     # print shape of data frame
-    #print(data.shape)
     # first column contains labels
     labels = data[:, 0]
-    #print(labels)
 
     # remaining columns are features
     features = data[:, 1:]
-    #print(features)
 
     return labels, features
 
 
-def nearest_neighbor(labels, features):
+# calculate euclidean distance between 2 points
+def euclidean_distance(point1, point2):
+    return np.sqrt(np.sum((point1 - point2) ** 2))
 
-    return 0
+
+def leave_one_out_cross_validation(labels, features, current_set_of_features, feature_considered):
+    # make a copy of features
+    features_with_current_set = features.copy()
+    # shape of features array
+    num_of_instances = features.shape[0]
+    num_of_features = features.shape[1]
+
+    if feature_considered not in current_set_of_features:
+        # traverse features, setting non-used features to 0
+        # forward selection
+        for i in range(num_of_features):
+            if i not in current_set_of_features and i != feature_considered:
+                features_with_current_set[:, i] = 0
+    else:
+        # traverse features, setting non-used features to 0
+        # backward elimination
+        # set feature_considered column to 0, as if you're removing it from current_set_of_features
+        for i in range(num_of_features):
+            if i not in current_set_of_features or i == feature_considered:
+                features_with_current_set[:, i] = 0
+
+    # initialize number of correctly classified object
+    number_correctly_classified = 0
+
+    # loop through all instances
+    for i in range(num_of_instances):
+        # store object to be classified
+        object_to_classify = features_with_current_set[i]
+        # get the label of the object
+        label_object_to_classify = labels[i]
+
+        # initialize distance and location to infinity
+        nearest_neighbor_distance = float('inf')
+        nearest_neighbor_location = float('inf')
+        # initialize label of nearest neighbor
+        nearest_neighbor_label = 0
+        # loop through number of instances
+        for k in range(num_of_instances):
+            # don't consider itself as its nearest neighbor
+            if k != i:
+                # get the kth instance
+                training_instance = features_with_current_set[k]
+                # calculate distance between 2 objects
+                distance = euclidean_distance(object_to_classify, training_instance)
+                # keep track of minimum distance
+                if distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = distance
+                    # get the location and label of nearest neighbor
+                    nearest_neighbor_location = k
+                    nearest_neighbor_label = labels[nearest_neighbor_location]
+
+        # if object is correctly classified, increment number_correctly_classified
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correctly_classified += 1
+
+    # calculate accuracy by dividing number correctly classified by total number of instances
+    return number_correctly_classified / num_of_instances
 
 
 def forward_selection(labels, features):
-    return 0
+    # initialize empty set of current features
+    current_set_of_features = []
+    # initialize empty set of best overall features
+    best_overall_set_of_features = []
+    # overall accuracy of best overall features
+    best_overall_accuracy = leave_one_out_cross_validation(labels, features, current_set_of_features, None)
+    # number of features in dataset
+    num_of_features = features.shape[1]
+
+    # calculate accuracy of empty set of features
+    print(f"\nBeginning Forward Selection search")
+    print(
+        f"Accuracy of current feature set {[f + 1 for f in current_set_of_features]}: {100 * best_overall_accuracy:.2f}%")
+
+    # loop through features
+    for i in range(num_of_features):
+        print(f"\nOn the {i + 1}th level of the search tree")
+        # initialize variable to keep track of which feature to add at ith level
+        feature_to_add_at_this_level = None
+        # keep track of best accuracy at current level
+        best_accuracy_at_current_level = 0
+
+        # loop through features
+        for k in range(num_of_features):
+            # if k feature is not already in current set of features, consider adding it
+            if k not in current_set_of_features:
+                # measure accuracy if you added k
+                accuracy = leave_one_out_cross_validation(labels, features, current_set_of_features, k)
+                print(f"--Considering adding the {k + 1} feature. Accuracy is {100 * accuracy:.2f}%")
+
+                if accuracy > best_accuracy_at_current_level:
+                    # update best accuracy at current level
+                    best_accuracy_at_current_level = accuracy
+                    # add k feature
+                    feature_to_add_at_this_level = k
+
+        # add k feature to current set of features
+        current_set_of_features.append(feature_to_add_at_this_level)
+        print(f"On level {i + 1}, I added feature {feature_to_add_at_this_level + 1} to current set")
+        print(f"Accuracy of current feature set {[f + 1 for f in current_set_of_features]}: {100*best_accuracy_at_current_level:.2f}%")
+
+        # track best overall feature set
+        if best_accuracy_at_current_level > best_overall_accuracy:
+            best_overall_accuracy = best_accuracy_at_current_level
+            best_overall_set_of_features = current_set_of_features.copy()
+        elif best_accuracy_at_current_level < best_overall_accuracy and i != num_of_features - 1:
+            print("WARNING: Accuracy has decreased. Continuing search in case of local maxima")
+
+    print(f"\nBest set of features: {[f + 1 for f in best_overall_set_of_features]}")
+    print(f"Overall accuracy: {100*best_overall_accuracy:.2f}%")
+
+    return best_overall_set_of_features
 
 def backward_elimination(labels, features):
-    return 0
+    # number of features in dataset
+    num_of_features = features.shape[1]
+    # initialize full set of current features
+    current_set_of_features = list(range(num_of_features))
+    # initialize set of best overall features, copying current set
+    best_overall_set_of_features = current_set_of_features.copy()
+    # overall accuracy of all features
+    best_overall_accuracy = leave_one_out_cross_validation(labels, features, current_set_of_features, None)
+
+    # calculate accuracy of full set of features
+    print(f"\nBeginning Backward Elimination search")
+    print(f"Accuracy of current feature set {[f + 1 for f in current_set_of_features]}: {100 * best_overall_accuracy:.2f}%")
+
+    # loop backwards through search tree
+    for i in range(num_of_features, 0, -1):
+        print(f"\nOn the {i}th level of the search tree")
+        # initialize variable to keep track of which feature to remove at ith level
+        feature_to_remove_at_this_level = None
+        # keep track of best accuracy at current level
+        best_accuracy_at_current_level = 0
+
+        # loop through features
+        for k in range(num_of_features):
+            # if k feature is already in current set of features, consider removing it
+            if k in current_set_of_features:
+                # measure accuracy if you removed k
+                accuracy = leave_one_out_cross_validation(labels, features, current_set_of_features, k)
+                print(f"--Considering removing the {k + 1} feature. Accuracy is {100 * accuracy:.2f}%")
+
+                if accuracy > best_accuracy_at_current_level:
+                    # update best accuracy at current level
+                    best_accuracy_at_current_level = accuracy
+                    # remove k feature
+                    feature_to_remove_at_this_level = k
+
+        # add k feature to current set of features
+        current_set_of_features.remove(feature_to_remove_at_this_level)
+        print(f"On level {i}, I removed feature {feature_to_remove_at_this_level + 1} from current set")
+        print(f"Accuracy of current feature set {[f + 1 for f in current_set_of_features]}: {100 * best_accuracy_at_current_level:.2f}%")
+
+        # track best overall feature set
+        if best_accuracy_at_current_level > best_overall_accuracy:
+            best_overall_accuracy = best_accuracy_at_current_level
+            best_overall_set_of_features = current_set_of_features.copy()
+        elif best_accuracy_at_current_level < best_overall_accuracy and i != 0:
+            print("WARNING: Accuracy has decreased. Continuing search in case of local maxima")
+
+    print(f"\nBest set of features: {[f + 1 for f in best_overall_set_of_features]}")
+    print(f"Overall accuracy: {100 * best_overall_accuracy:.2f}%")
+
+    return best_overall_set_of_features
 
 def main():
     small_data = 'data/CS170_Small_Data__85.txt'
     large_data = 'data/CS170_Large_Data__67.txt'
-    labels, features = 0, 0
+    labels, features = None, None
 
     # choose a dataset to load
     while True:
@@ -44,17 +202,20 @@ def main():
         # load the corresponding dataset
         if choice == '1':
             labels, features = load_data(small_data)
-            print(f"\nSmall dataset loaded successfully. Dataset contains {features.shape[0]} rows "
-                  f"and {features.shape[1]} features.")
+            print(f"\nSmall dataset loaded successfully. Dataset contains {features.shape[0]} instances "
+                  f"and {features.shape[1]} features (not including the class attribute).")
             break
         elif choice == '2':
             labels, features = load_data(large_data)
-            print(f"\nLarge dataset loaded successfully. Dataset contains {features.shape[0]} rows "
-                  f"and {features.shape[1]} features.")
+            print(f"\nLarge dataset loaded successfully. Dataset contains {features.shape[0]} instances "
+                  f"and {features.shape[1]} features (not including the class attribute).")
             break
         else:
             # ask again for user input if invalid
             print("Invalid Input. Please enter 1 or 2 depending on which dataset you want to load.\n")
+
+    # time stamp for calculating duration of runtime
+    start_time = time.time()
 
     # choose which kind of search. forward or backward
     while True:
@@ -62,6 +223,7 @@ def main():
         # ask user which feature search they want to use
         choice = input("Enter the number next to the corresponding feature search you want to use: ")
 
+        start_time = time.time()
         # execute corresponding search
         if choice == '1':
             forward_selection(labels, features)
@@ -73,7 +235,8 @@ def main():
             # ask again for user input if invalid
             print("Invalid Input. Please enter 1 or 2 depending on which search you want to use.\n")
 
-
+    end_time = time.time()
+    print(f"Total runtime = {end_time - start_time:.2f} seconds")
 
     return 0
 
